aa48118a6f302faa078cfee30b60e7b990480e73
yangnoiceman <yangnooiceman@gmail.com>
2019-10-17 19:45:24 +0800

    add index and bootsrap

--- src/main/java/study/yang/personal/Controller/IndexController.java
+++ src/main/java/study/yang/personal/Controller/IndexController.java
@@ -10,11 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  */
 @Controller
-public class HelloConrtoller {
-    @GetMapping("/hello")
-    public  String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-    return "hello";
-
-    }
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
 }
